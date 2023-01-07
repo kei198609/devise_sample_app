1d6c041e2be153ca47ef2d3d3dbee60cea25d994
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-01-07 13:17:17 +0900

    deviseコントローラ作成 画面遷移成功

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/home'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+    }
+
+  devise_scope :user do
+  get 'pages/home' => 'users/registrations#home'
+  end
+
+  def devise_scope(scope)
+    constraint = lambda do |request|
+      request.env["devise.mapping"] = Devise.mappings[scope]
+      true
+    end
+
+    constraints(constraint) do
+      yield
+    end
+  end
+
 end
